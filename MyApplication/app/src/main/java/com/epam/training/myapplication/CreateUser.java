acd68c267e8d923fe30deb4ecbbcd564bc226dcf
Vsevolod Lomakin <bloodseva@ya.ru>
2014-10-08 15:44:39 +0300

    Added new activities, text returns to main activiti, onsaveinstancesave added.

---
+++ MyApplication/app/src/main/java/com/epam/training/myapplication/CreateUser.java
@@ -0,0 +1,33 @@
+package com.epam.training.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+
+public class CreateUser extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_user);
+    }
+
+    public void onOkClick(View view) {
+        Intent UserInformation = new Intent();
+        EditText eLogin = (EditText)findViewById(R.id.Login);
+        EditText ePassword = (EditText)findViewById(R.id.Pass);
+        UserInformation.putExtra("Login", eLogin.getText().toString());
+        UserInformation.putExtra("Password", ePassword.getText().toString());
+        setResult(RESULT_OK, UserInformation);
+        finish();
+    }
+
+    public void onCancelClick(View view) {
+        finish();
+    }
+}
--- MyApplication/app/src/main/java/com/epam/training/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/epam/training/myapplication/MainActivity.java
@@ -1,17 +1,62 @@
 package com.epam.training.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private static final int REQUEST_LOGIN = 0;
+
+    public int attempts = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tView= (TextView)findViewById(R.id.Attempts);
+        tView.setText(new String().valueOf(attempts));
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_LOGIN && resultCode == RESULT_OK) {
+            EditText Login = (EditText)findViewById(R.id.Login);
+            EditText Password = (EditText)findViewById(R.id.Pass);
+            Login.setText(data.getStringExtra("Login"));
+            Password.setText(data.getStringExtra("Password"));
+        }
+    }
+
+    public void onLoginClick(View view) {
+        attempts++;
+        TextView tView= (TextView)findViewById(R.id.Attempts);
+        tView.setText(new String().valueOf(attempts));
+    }
+
+    public void onCreateClick(View view) {
+        Intent intent = new Intent(this, CreateUser.class);
+        startActivityForResult(intent, REQUEST_LOGIN);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putInt("Attempts",attempts);
+        super.onSaveInstanceState(savedInstanceState);
+
+    }
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        attempts=savedInstanceState.getInt("Attempts");
+        TextView tView= (TextView)findViewById(R.id.Attempts);
+        tView.setText(new String().valueOf(attempts));
+    }
 }
